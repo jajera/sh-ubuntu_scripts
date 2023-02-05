1a1e8ac06c5616eb1e8797464b1d1622564b0cbc
John Ajera <37360952+jajera@users.noreply.github.com>
2023-02-05 18:26:17 +0800

    initial commit

---
+++ install-kubectl.sh
@@ -0,0 +1,7 @@
+# download and install
+sudo curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# verify
+kubectl version --client
+which kubectl

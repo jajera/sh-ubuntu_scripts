eb11918ce23b4d993f7a9f1731e39b34e582edb8
John Ajera <37360952+jajera@users.noreply.github.com>
2023-03-05 14:55:16 +0800

    Update install-azurecli.sh

--- install-azurecli.sh
+++ install-azurecli.sh
@@ -3,14 +3,12 @@ sudo apt-get update
 sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 
  # set microsoft key
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo 'deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main' | sudo tee /etc/apt/sources.list.d/vscode.list
-rm -f packages.microsoft.gpg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 # install azurecli
-sudo apt-get update
-sudo apt-get install -y azure-cli
+sudo apt -y update
+sudo apt install -y azure-cli
 
 # verify
 az --version
